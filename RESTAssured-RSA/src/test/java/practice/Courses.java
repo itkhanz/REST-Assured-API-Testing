0002d33d6b35340c75e3e6aa2e8d6ed98428480a
itkhan <itkhanz@gmail.com>
2023-03-13 13:22:28 +0500

    Diving in Depth-Automating REST APIs

---
+++ RESTAssured-RSA/src/test/java/practice/Courses.java
@@ -0,0 +1,62 @@
+package practice;
+
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import practice.files.Payload;
+import practice.utils.ParseUtils;
+
+/****************************************
+ * 1. Print no of courses returned by API
+ * 2. Print purchase amount
+ * 3. Print title of the first course
+ * 4. Print all course titles and their respective prices
+ * 5. Print no of copies sold by RPA Course
+ * 6. Verify if sum of all Course prices matches with purchase amount
+ */
+public class Courses {
+
+    public static JsonPath parsedCoursesResponse = ParseUtils.rawToJson(Payload.coursePrice());
+
+    @Test
+    public void test_Courses() {
+        //Print no of courses returned by API
+        int count = parsedCoursesResponse.getInt("courses.size()");
+        System.out.println(count);  //3
+
+        //Print purchase amount
+        int totalAmount = parsedCoursesResponse.getInt("dashboard.purchaseAmount");
+        System.out.println(totalAmount);    //910
+
+        //Print title of the first course
+        String titleFirstCourse = parsedCoursesResponse.getString("courses[2].title");
+        System.out.println(titleFirstCourse);   //RPA
+
+        //Print all course titles and their respective prices
+        for (int i = 0; i < count; i++) {
+            String courseTitles = parsedCoursesResponse.get("courses[" + i + "].title");
+            System.out.println("title: " + courseTitles);
+            System.out.println("price: " + parsedCoursesResponse.get("courses[" + i + "].price").toString());
+        }
+
+        //Print no of copies sold by RPA Course
+        for (int i = 0; i < count; i++) {
+            String courseTitles = parsedCoursesResponse.get("courses[" + i + "].title");
+            if (courseTitles.equalsIgnoreCase("RPA")) {
+                System.out.println("copies sold by RPA: " + parsedCoursesResponse.get("courses[" + i + "].copies").toString()); //10
+                break;
+            }
+        }
+
+        //Verify if sum of all Course prices matches with purchase amount
+        //(50*6)+(40*4)+(45*10) = 910
+        int actualSum = 0;
+        for (int i = 0; i < count; i++) {
+            int coursePrice = parsedCoursesResponse.getInt("courses[" + i + "].price");
+            int courseCopies = parsedCoursesResponse.getInt("courses[" + i + "].copies");
+            actualSum = actualSum+(coursePrice*courseCopies);
+        }
+        System.out.println(actualSum);
+        Assert.assertEquals(actualSum, totalAmount);
+    }
+}
--- RESTAssured-RSA/src/test/java/practice/files/Payload.java
+++ RESTAssured-RSA/src/test/java/practice/files/Payload.java
@@ -31,4 +31,30 @@ public class Payload {
                 "\"key\":\"qaclick123\"\n" +
                 "}\n";
     }
+
+    public static String coursePrice() {
+        return "{\n" +
+                "\t\"dashboard\": {\n" +
+                "\t\t\"purchaseAmount\": 910,\n" +
+                "\t\t\"website\": \"rahulshettyacademy.com\"\n" +
+                "\t},\n" +
+                "\t\"courses\": [\n" +
+                "\t\t{\n" +
+                "\t\t\t\"title\": \"Selenium Python\",\n" +
+                "\t\t\t\"price\": 50,\n" +
+                "\t\t\t\"copies\": 6\n" +
+                "\t\t},\n" +
+                "\t\t{\n" +
+                "\t\t\t\"title\": \"Cypress\",\n" +
+                "\t\t\t\"price\": 40,\n" +
+                "\t\t\t\"copies\": 4\n" +
+                "\t\t},\n" +
+                "\t\t{\n" +
+                "\t\t\t\"title\": \"RPA\",\n" +
+                "\t\t\t\"price\": 45,\n" +
+                "\t\t\t\"copies\": 10\n" +
+                "\t\t}\n" +
+                "\t]\n" +
+                "}";
+    }
 }
