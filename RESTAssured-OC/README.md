ecbfe20ce82b368c9ecf1f7476f37792ea180e55
itkhan <itkhanz@gmail.com>
2023-05-16 21:27:14 +0200

    Assignment 3 solution

--- RESTAssured-OC/README.md
+++ RESTAssured-OC/README.md
@@ -1341,7 +1341,56 @@ public void validate_response_body(){
 
 #### Fetch all nested Keys and values from a given JSON Array
 
+```java
+public static List<String> allKeysList = new ArrayList<String>();
+    public static List<Object> allValuesList = new ArrayList<Object>();
+
+    /**
+     * Given below JSON array, fetch all nested keys and values.
+     *
+     * Store the keys in an array list.
+     * Store the values in an array list.
+     *
+     * Final output should be,
+     * keys = [k, k10, k11, k121, k120, k12, k14, k1, k221, k22]
+     * values = [[1, 3, 5], 4, [4, 7, 9], v121, 6, v122]
+     * This should work even if the number of nested objects vary.
+     * @throws JsonProcessingException
+     */
+    @Test
+    public void exe3_fetch_nested_keys_values() throws IOException {
+
+        File jsonFile = new File("src\\test\\java\\com\\itkhan\\practice\\codingChallenges\\assignment3.json");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArrayList<Map<String, Object>> jsonArr = objectMapper.readValue(jsonFile,
+                new TypeReference<ArrayList<Map<String, Object>>>(){}
+        );
+
+        for (Map<String, Object> jsonMap : jsonArr) {
+            iterate_over(jsonMap);
+        }
+
+        System.out.println(allKeysList.toString()); //[k, k1, k10, k11, k12, k120, k121, k14, k22, k221]
+        System.out.println(allValuesList.toString()); //[[1, 3, 5], 4, [4, 7, 9], v121, 6, v122]
+    }
+
+    public void iterate_over(Map<String, Object> map) {
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            //System.out.println("Key is: " + entry.getKey());
+            allKeysList.add(entry.getKey());
+            if (entry.getValue() instanceof Map) {
+                //System.out.println("Map found, digging further");
+                iterate_over((Map<String, Object>) entry.getValue());
+            } else {
+                //System.out.println("Leaf found, value is: " + entry.getValue());
+                allValuesList.add(entry.getValue());
+            }
+        }
+    }
+```
 #### Fetch values of fields with same key name
+
 
 ### Authentication and Authorization
 
---
+++ RESTAssured-OC/src/test/java/com/itkhan/practice/codingChallenges/Exercises.java
@@ -0,0 +1,64 @@
+package com.itkhan.practice.codingChallenges;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+
+public class Exercises {
+
+    public static List<String> allKeysList = new ArrayList<String>();
+    public static List<Object> allValuesList = new ArrayList<Object>();
+
+    /**
+     * Given below JSON array, fetch all nested keys and values.
+     *
+     * Store the keys in an array list.
+     * Store the values in an array list.
+     *
+     * Final output should be,
+     * keys = [k, k10, k11, k121, k120, k12, k14, k1, k221, k22]
+     * values = [[1, 3, 5], 4, [4, 7, 9], v121, 6, v122]
+     * This should work even if the number of nested objects vary.
+     * @throws JsonProcessingException
+     */
+    @Test
+    public void exe3_fetch_nested_keys_values() throws IOException {
+
+        File jsonFile = new File("src\\test\\java\\com\\itkhan\\practice\\codingChallenges\\assignment3.json");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArrayList<Map<String, Object>> jsonArr = objectMapper.readValue(jsonFile,
+                new TypeReference<ArrayList<Map<String, Object>>>(){}
+        );
+
+        for (Map<String, Object> jsonMap : jsonArr) {
+            iterate_over(jsonMap);
+        }
+
+        System.out.println(allKeysList.toString()); //[k, k1, k10, k11, k12, k120, k121, k14, k22, k221]
+        System.out.println(allValuesList.toString()); //[[1, 3, 5], 4, [4, 7, 9], v121, 6, v122]
+    }
+
+    public void iterate_over(Map<String, Object> map) {
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            //System.out.println("Key is: " + entry.getKey());
+            allKeysList.add(entry.getKey());
+            if (entry.getValue() instanceof Map) {
+                //System.out.println("Map found, digging further");
+                iterate_over((Map<String, Object>) entry.getValue());
+            } else {
+                //System.out.println("Leaf found, value is: " + entry.getValue());
+                allValuesList.add(entry.getValue());
+            }
+        }
+    }
+
+
+
+}
